81641223b32f5956a6c68498615c566d57813fc6
deni1804 <denizulkarnaen223@gmail.com>
2022-07-13 17:01:15 +0700

    firs

---
+++ application/views/templates/templates-user/modal.php
@@ -0,0 +1,76 @@
+<!-- login modal -->
+<div class="modal fade" tabindex="-1" id="loginModal" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Login Member</h5>
+                <button type="button" class="close" data-dismis="modal" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('member'); ?>" method="POST">
+                <div class="modal-body">
+                    <div class="form-group row">
+                        <label for="email" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="email" placeholder="Alamat Email">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" id="password" placeholder="Password">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-dismis="modal">Close</button>
+                    <button type="submit" class="btn btn-outline-primary">Log in</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- login modal -->
+
+<!-- daftar modal -->
+<div class="modal fade" tabindex="-1" id="daftarModal" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Daftar Anggota</h5>
+                <button type="button" class="close" data-dismis="modal" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('member/daftar'); ?>" method="POST">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-user" id="nama" name="nama" placeholder="Nama Lengkap">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-user" name="alamat" id="alamat" placeholder="Alamat Lengkap">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Alamat Email">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Ulangi Password">
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-dismis="modal">Close</button>
+                    <button type="submit" class="btn btn-outline-primary">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- end modal daftar -->
+
+<!-- modal info -->
+<div class="modal fade" tabindex="-1" id=""></div>
